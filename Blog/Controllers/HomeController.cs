c2a2ac15e49729262e4d1fb8211b9059f8d1ee9b
TheNickDeveloper <3235526751@qq.com>
2020-01-06 18:40:33 +0800

    initial commit
    1. Set up controllers, models, and View
    2. Basic view establish
    3. Local Db establish

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _ctx;
+
+        public HomeController(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _ctx.Posts.Add(post);
+            await _ctx.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Blog
+ Self build Blog practice by using ASP.Net
